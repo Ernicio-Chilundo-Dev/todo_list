2ba8ec6ceb4366ac1d989a3e1597f67e2b3264f4
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-10-12 00:39:31 +0200

    para funcional feita

--- javascript.js
+++ javascript.js
@@ -5,10 +5,10 @@ const addTaskButton = document.getElementById("add-task")
 const taskList = document.getElementById("task-list")
 
 // funcao para adicionar uma nova funcao
-function addTask(){
+function addTask() {
     const taskText = taskInput.value
 
-    if(taskText == ''){
+    if (taskText == '') {
         alert("Please enter a task!")
         return;
     }
@@ -16,4 +16,34 @@ function addTask(){
     const taskItem = document.createElement("li")
     taskItem.innerHTML = `${taskText}
     <button class="delete-btn">Delete</button>`;
+
+    // Marcar como completado ao clicar
+    taskItem.addEventListener("click", function () {
+        taskItem.classList.toggle('completed');
+    });
+
+    //Remover tarefa
+
+    const deleteButton = taskItem.querySelector(".delete-btn")
+    deleteButton.addEventListener("click", function (event) {
+        taskList.removeChild(taskItem)
+        event.stopPropagation()
+    })
+
+    //Adicionar a tarefa a lista
+    taskList.appendChild(taskItem)
+
+    // Limpar input
+    taskInput.value = ""
 }
+
+// Evento listiner para o botao de adicionar tarefa
+addTaskButton.addEventListener("click", addTask)
+
+
+//Adicionar tarefa ao pressionar Enter
+taskInput.addEventListener("keypress", function (event) {
+    if (event.key == 'Enter') {
+        addTask()
+    }
+})
